23913e97937736aee3a6cf8432dfe824460f3aaf
christopher-glitch <christph.glitch@gmail.com>
2024-02-09 04:43:29 +0900

    complete home contents

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			PressStart: ['"Press Start 2P"', 'sans-serif'],
+			Roboto: ['Roboto', 'sans-serif'],
+			Menlo: ['Menlo', 'sans-serif'],
+			Monaco: ['Monaco', 'sans-serif'], 
+			japan: ['メイリオ', 'ヒラギノ丸ゴシック']
+		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
 }
